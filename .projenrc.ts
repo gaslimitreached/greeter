98eb14052cb8bf6ec5e8dbd516ee24c20c5c366a
cachemonet <90769841+gaslimitreached@users.noreply.github.com>
2023-07-03 12:55:18 -0500

    revert to npm

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,12 +2,12 @@ import { typescript } from 'projen';
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
-  releaseToNpm: true,
   name: '@cachemonet/greeter',
-  repository: 'https://github.com/gaslimitreached/greeter.git',
-  npmRegistryUrl: 'https://npm.pkg.github.com',
-  projenrcTs: true,
+  // npmRegistryUrl: 'https://npm.pkg.github.com',
   packageName: 'greeter',
+  projenrcTs: true,
+  releaseToNpm: true,
+  repository: 'https://github.com/gaslimitreached/greeter.git',
 });
 
 project.synth();
