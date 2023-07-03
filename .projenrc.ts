bdd69c81a3c56ad15aab20a2fddfa934fcfa7328
cachemonet <90769841+gaslimitreached@users.noreply.github.com>
2023-07-03 13:43:20 -0500

    try scope npm user

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,9 +3,7 @@ import { NpmAccess } from 'projen/lib/javascript';
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
-  // https://stackoverflow.com/questions/64589655/package-is-not-publishing-to-npm-not-in-the-npm-registry
-  name: '@gaslimitreached/greeter',
-  npmRegistryUrl: 'https://npm.pkg.github.com',
+  name: '@cachemonet/greeter',
   npmAccess: NpmAccess.PUBLIC,
   projenrcTs: true,
   releaseToNpm: true,
