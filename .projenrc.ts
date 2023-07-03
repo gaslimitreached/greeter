dfe171401f4cdc759e2e33aff3b7de5850f836a3
cachemonet <90769841+gaslimitreached@users.noreply.github.com>
2023-07-03 13:26:40 -0500

    scope package name

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,18 +3,13 @@ import { NpmAccess } from 'projen/lib/javascript';
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
+  // https://stackoverflow.com/questions/64589655/package-is-not-publishing-to-npm-not-in-the-npm-registry
   name: '@gaslimitreached/greeter',
   npmRegistryUrl: 'https://npm.pkg.github.com',
   npmAccess: NpmAccess.PUBLIC,
-  packageName: 'greeter',
   projenrcTs: true,
   releaseToNpm: true,
   repository: 'https://github.com/gaslimitreached/greeter.git',
-  // only supported for aws code artifacts
-  // scopedPackagesOptions: [{
-  //   scope: '@cachemonet',
-  //   registryUrl: 'https://registry.npmjs.org/',
-  // }],
 });
 
 project.synth();
