a750aba4554bce2b785b83b3072a55fe429e5598
cachemonet <90769841+gaslimitreached@users.noreply.github.com>
2023-07-03 13:19:58 -0500

    github package

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,20 @@
 import { typescript } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
-  name: '@cachemonet/greeter',
-  // npmRegistryUrl: 'https://npm.pkg.github.com',
+  name: '@gaslimitreached/greeter',
+  npmRegistryUrl: 'https://npm.pkg.github.com',
+  npmAccess: NpmAccess.PUBLIC,
   packageName: 'greeter',
   projenrcTs: true,
   releaseToNpm: true,
   repository: 'https://github.com/gaslimitreached/greeter.git',
+  // only supported for aws code artifacts
+  // scopedPackagesOptions: [{
+  //   scope: '@cachemonet',
+  //   registryUrl: 'https://registry.npmjs.org/',
+  // }],
 });
 
 project.synth();
